31c3a485b145f22df3f6759fa3a9cbd745294896
linfuqing <linfuqing@126.com>
2024-04-07 21:38:20 +0800

    version 2.10.15

--- Game.Entities/Actors/GameRangeSpawnerComponent.cs
+++ Game.Entities/Actors/GameRangeSpawnerComponent.cs
@@ -1,18 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System;
+using ZG;
 
-public class GameRangeSpawnerComponent : MonoBehaviour
+[EntityComponent(typeof(GameRangeSpawnerNode))]
+[EntityComponent(typeof(GameRangeSpawnerStatus))]
+public class GameRangeSpawnerComponent : EntityProxyComponent, IEntityComponent
 {
-    // Start is called before the first frame update
-    void Start()
+    [Serializable]
+    public struct Node
     {
+#if UNITY_EDITOR
+        public string name;
+#endif
         
+        public int sliceIndex;
     }
 
-    // Update is called once per frame
-    void Update()
+    public Node[] nodes;
+    
+    void IEntityComponent.Init(in Unity.Entities.Entity entity, EntityComponentAssigner assigner)
     {
+        int numNodes = this.nodes == null ? 0 : this.nodes.Length;
+        var nodes = new GameRangeSpawnerNode[numNodes];
+        for (int i = 0; i < numNodes; ++i)
+            nodes[i].sliceIndex = this.nodes[i].sliceIndex;
         
+        assigner.SetBuffer(EntityComponentAssigner.BufferOption.Override, entity, nodes);
     }
 }
